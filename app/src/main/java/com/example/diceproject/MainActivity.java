92fae36bf7b34548fff20e0cc16168c46bc66acf
Zachary ElledgeMCGhee <zte18a@acu.edu>
2020-09-14 21:51:48 -0500

    set up switch cases and edited filenames of pics

--- app/src/main/java/com/example/diceproject/MainActivity.java
+++ app/src/main/java/com/example/diceproject/MainActivity.java
@@ -37,51 +37,51 @@ public class MainActivity extends AppCompatActivity {
 
     private void rollDice() {
 
-        int randomNumber = rng.nextInt(6) + 1;
+        int randomNumber = rng.nextInt(20) + 1;
         final MediaPlayer mediaPlayer;
         mediaPlayer = MediaPlayer.create(this, R.raw.diceroll);
 
         switch (randomNumber){
             case 1:
-                imageViewDice.setImageResource(R.drawable.dice1);
+                imageViewDice.setImageResource(R.drawable.d1);
             case 2:
-                imageViewDice.setImageResource(R.drawable.dice2);
+                imageViewDice.setImageResource(R.drawable.d2);
             case 3:
-                imageViewDice.setImageResource(R.drawable.dice3);
+                imageViewDice.setImageResource(R.drawable.d3);
             case 4:
-                imageViewDice.setImageResource(R.drawable.dice4);
+                imageViewDice.setImageResource(R.drawable.d4);
             case 5:
-                imageViewDice.setImageResource(R.drawable.dice5);
+                imageViewDice.setImageResource(R.drawable.d5);
             case 6:
-                imageViewDice.setImageResource(R.drawable.dice6);
+                imageViewDice.setImageResource(R.drawable.d6);
             case 7:
-                imageViewDice.setImageResource(R.drawable.dice7);
+                imageViewDice.setImageResource(R.drawable.d7);
             case 8:
-                imageViewDice.setImageResource(R.drawable.dice8);
+                imageViewDice.setImageResource(R.drawable.d8);
             case 9:
-                imageViewDice.setImageResource(R.drawable.dice9);
+                imageViewDice.setImageResource(R.drawable.d9);
             case 10:
-                imageViewDice.setImageResource(R.drawable.dice10);
+                imageViewDice.setImageResource(R.drawable.d10);
             case 11:
-                imageViewDice.setImageResource(R.drawable.dice11);
+                imageViewDice.setImageResource(R.drawable.d11);
             case 12:
-                imageViewDice.setImageResource(R.drawable.dice12);
+                imageViewDice.setImageResource(R.drawable.d12);
             case 13:
-                imageViewDice.setImageResource(R.drawable.dice13);
+                imageViewDice.setImageResource(R.drawable.d13);
             case 14:
-                imageViewDice.setImageResource(R.drawable.dice14);
+                imageViewDice.setImageResource(R.drawable.d14);
             case 15:
-                imageViewDice.setImageResource(R.drawable.dice15);
+                imageViewDice.setImageResource(R.drawable.d15);
             case 16:
-                imageViewDice.setImageResource(R.drawable.dice16);
+                imageViewDice.setImageResource(R.drawable.d16);
             case 17:
-                imageViewDice.setImageResource(R.drawable.dice17);
+                imageViewDice.setImageResource(R.drawable.d17);
             case 18:
-                imageViewDice.setImageResource(R.drawable.dice18);
+                imageViewDice.setImageResource(R.drawable.d18);
             case 19:
-                imageViewDice.setImageResource(R.drawable.dice19);
+                imageViewDice.setImageResource(R.drawable.d19);
             case 20:
-                imageViewDice.setImageResource(R.drawable.dice20);
+                imageViewDice.setImageResource(R.drawable.d20);
         }
     }
 }
