f878be3938d254871f1dac59bfcf9f2a2d35a13f
Zachary ElledgeMCGhee <zte18a@acu.edu>
2020-09-14 21:42:18 -0500

    Added the images for 20 die

--- app/src/main/java/com/example/diceproject/MainActivity.java
+++ app/src/main/java/com/example/diceproject/MainActivity.java
@@ -2,32 +2,44 @@ package com.example.diceproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ImageView;
 
 import java.util.Random;
 
+import static com.example.diceproject.R.raw.diceroll;
+
 public class MainActivity extends AppCompatActivity {
     private ImageView imageViewDice;
     private Random rng = new Random();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         imageViewDice = findViewById(R.id.image_view_dice);
+        final MediaPlayer mediaPlayer;
+            mediaPlayer = MediaPlayer.create(this, R.raw.diceroll);
         imageViewDice.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 rollDice();
             }
         });
+        }
 
-    }
+
     private void rollDice() {
-        int randomNumber = rng.nextInt(6) +1;
+
+        int randomNumber = rng.nextInt(6) + 1;
+        final MediaPlayer mediaPlayer;
+        mediaPlayer = MediaPlayer.create(this, R.raw.diceroll);
 
         switch (randomNumber){
             case 1:
